9846076c006fe8aa7779fabea59005f53b4fc0b7
pengefutar <siri.lukacs@gmail.com>
2017-05-29 13:48:51 +0200

    project structure initiated, MinesweeperTable added

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by eszti on 2017.05.29..
+ */
+public class Main {
+    public static void main(String[] args) {
+        MinesweeperTable ms = new MinesweeperTable(9, 9, 15);
+        ms.doit();
+    }
+}
